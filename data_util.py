9565865a113378ddaca916047c5a6e12d2c298bf
Jerome de Dios <182743031+jdedios-de@users.noreply.github.com>
2024-11-04 18:05:14 +0100

    commit final and include test and github

--- data_util.py
+++ data_util.py
@@ -8,6 +8,23 @@ cached_data_html = None
 
 
 def load_data_json(file_path: WindowsPath) -> misc_util.result_message:
+    """
+    Loads data from a JSON file and returns a result message with the
+    file's contents.
+
+    Parameter:
+        file_path (WindowsPath): The path to the JSON file to load.
+
+    Returns:
+        misc_util.result_message: A structured message indicating
+        success or failure, with the JSON payload if successful,
+        or an error message if not.
+
+    Raises:
+        FileNotFoundError: If the file cannot be located.
+        IOError: If there is an issue reading the file.
+        Exception: For any other unexpected errors encountered.
+    """
     try:
         with open(file_path, "r") as handle:
             payload = json.load(handle)
@@ -31,6 +48,23 @@ def load_data_json(file_path: WindowsPath) -> misc_util.result_message:
 
 
 def load_data_html(file_path: WindowsPath) -> misc_util.result_message:
+    """
+    Loads data from an HTML file and returns a result message with the
+    file's contents.
+
+    Parameter:
+        file_path (WindowsPath): The path to the HTML file to load.
+
+    Returns:
+        misc_util.result_message: A structured message indicating
+        success or failure, with the HTML content as a list if successful,
+        or an error message if not.
+
+    Raises:
+        FileNotFoundError: If the file cannot be located.
+        IOError: If there is an issue reading the file.
+        Exception: For any other unexpected errors encountered.
+    """
     try:
         with open(file_path, "r") as handle:
             payload = handle.readlines()
@@ -54,6 +88,19 @@ def load_data_html(file_path: WindowsPath) -> misc_util.result_message:
 
 
 def fetch_data_json(file_path: WindowsPath) -> misc_util.result_message:
+    """
+    Fetches cached JSON data if available; otherwise, loads it from file.
+
+    Uses a cached version of the JSON data if present; if not, loads the data
+    from the specified file path and caches it for future access.
+
+    Parameter:
+        file_path (WindowsPath): The path to the JSON file.
+
+    Returns:
+        misc_util.result_message: A result message containing the JSON
+        data if successful, or an error message if not.
+    """
     global cached_data_json
     if cached_data_json is None:
         cached_data_json = load_data_json(file_path)
@@ -61,6 +108,19 @@ def fetch_data_json(file_path: WindowsPath) -> misc_util.result_message:
 
 
 def fetch_data_html(file_path: WindowsPath) -> misc_util.result_message:
+    """
+    Fetches cached HTML data if available; otherwise, loads it from file.
+
+    Uses a cached version of the HTML data if present; if not, loads the data
+    from the specified file path and caches it for future access.
+
+    Args:
+        file_path (WindowsPath): The path to the HTML file.
+
+    Returns:
+        misc_util.result_message: A result message containing the HTML
+        data if successful, or an error message if not.
+    """
     global cached_data_html
     if cached_data_html is None:
         cached_data_html = load_data_html(file_path)
@@ -69,6 +129,23 @@ def fetch_data_html(file_path: WindowsPath) -> misc_util.result_message:
 
 def write_data(details: str,
                file_path: WindowsPath) -> misc_util.result_message:
+    """
+    Writes data to a specified file path and returns a result message.
+
+    Parameters:
+        details (str): The content to write to the file.
+        file_path (WindowsPath): The path where the data should be written.
+
+    Returns:
+        misc_util.result_message: A structured message indicating
+        success or failure, with a success message if written successfully
+        or an error message if not.
+
+    Raises:
+        FileNotFoundError: If the file cannot be found.
+        IOError: If there is an issue writing to the file.
+        Exception: For any other unexpected errors encountered.
+    """
     try:
         with open(file_path, 'w') as write_to_file:
             write_to_file.write(details)
