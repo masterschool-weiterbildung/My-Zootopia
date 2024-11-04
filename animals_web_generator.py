53b10adce388ed1d51d59ab489acec36470a2dca
Jerome de Dios <182743031+jdedios-de@users.noreply.github.com>
2024-11-04 02:01:58 +0100

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -4,11 +4,12 @@ import constant
 import data_util
 import print_util
 from data_util import write_data
-from misc_util import get_animals, replace_html_content
+from misc_util import get_animals, replace_html_content, \
+    replace_html_with_serialize_items
 
 
 def main():
-    write_data(replace_html_content(),constant.NEW_HTML_FILE_PATH)
+    write_data(replace_html_with_serialize_items(),constant.NEW_HTML_FILE_PATH)
 
 
 if __name__ == '__main__':
--- misc_util.py
+++ misc_util.py
@@ -21,9 +21,30 @@ def get_animals() -> str:
             continue
     return output
 
+def get_serialize_animals() -> str:
+    output = ''
+    for animal in data_util.fetch_data_json(constant.JSON_FILE_PATH)[
+        constant.PAYLOAD]:
+        try:
+            animal[constant.CHARACTERISTICS][constant.TYPE]
+            output += '<li class="cards__item">'
+            output += f"Name: {animal[constant.NAME]}<br/>\n"
+            output += f"Diet: {animal[constant.CHARACTERISTICS][constant.DIET]}<br/>\n"
+            output += f"Location: {animal[constant.LOCATIONS][0]}<br/>\n"
+            output += f"Type: {animal[constant.CHARACTERISTICS][constant.TYPE]}<br/>\n"
+        except KeyError as e:
+            continue
+    return output
+
 
 def replace_html_content() -> str:
     return_value = data_util.fetch_data_html(constant.HTML_FILE_PATH)[
         constant.PAYLOAD]
     return ''.join(return_value).replace("__REPLACE_ANIMALS_INFO__",
                                          get_animals())
+
+def replace_html_with_serialize_items() -> str:
+    return_value = data_util.fetch_data_html(constant.HTML_FILE_PATH)[
+        constant.PAYLOAD]
+    return ''.join(return_value).replace("__REPLACE_ANIMALS_INFO__",
+                                         get_serialize_animals())
